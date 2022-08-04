e8e005742ad6a80202f505d45b5d9622ff6db639
Yuuki Kunugi <yuuki@is-dmms-MacBook-Pro.local>
2022-08-04 12:30:33 +0900

    Revert "POST /statuses で正しいレスポンスが返ってくる"
    This reverts commit 8669201fc5b6c33d6c384b24bf41f008fdff1bd8.

--- app/dao/dao.go
+++ app/dao/dao.go
@@ -14,8 +14,6 @@ type (
 		// Get account repository
 		Account() repository.Account
 
-		Status() repository.Status
-
 		// Clear all data in DB
 		InitAll() error
 	}
@@ -38,10 +36,6 @@ func New(config DBConfig) (Dao, error) {
 
 func (d *dao) Account() repository.Account {
 	return NewAccount(d.db)
-}
-
-func (d *dao) Status() repository.Status {
-	return NewStatus(d.db)
 }
 
 func (d *dao) InitAll() error {
--- app/handler/router.go
+++ app/handler/router.go
@@ -7,7 +7,6 @@ import (
 	"yatter-backend-go/app/app"
 	"yatter-backend-go/app/handler/accounts"
 	"yatter-backend-go/app/handler/health"
-	"yatter-backend-go/app/handler/statuses"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -31,7 +30,6 @@ func NewRouter(app *app.App) http.Handler {
 
 	r.Mount("/v1/accounts", accounts.NewRouter(app))
 	r.Mount("/v1/health", health.NewRouter())
-	r.Mount("/v1/statuses", statuses.NewRouter(app))
 
 	return r
 }
